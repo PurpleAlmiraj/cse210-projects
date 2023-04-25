0a4e20ab83e04fe014830a865df38eaaed4a3df9
Ryan Jones <ryanbluej@gmail.com>
2023-04-25 09:13:47 -0700

    Completed prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,37 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your Grade Percentage? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        if (number >= 90)
+        {
+            Console.WriteLine("You got an A");
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine("You got a B");
+        }
+        else if (number >= 70)
+        {
+            Console.WriteLine("You got a C");
+        }
+        else if (number >= 60)
+        {
+            Console.WriteLine("You got a D");
+        }
+        else
+        {
+            Console.WriteLine("You got a F");
+        }
+        if (number < 60)
+        {
+            Console.WriteLine("You didn't pass the class");
+        }
+        if (number >= 60)
+        {
+            Console.WriteLine("You passed the class");
+        }
     }
 }
