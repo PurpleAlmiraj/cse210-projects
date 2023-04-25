a3c993e37817d0141fd3c78775c4e673b6903d81
Ryan Jones <ryanbluej@gmail.com>
2023-04-25 10:03:46 -0700

    Other three preps

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        int guess = 0;
+        Random randomGenerator = new Random();
+        int magicnumber = randomGenerator.Next(1, 100);
+        while (guess != magicnumber)
+        {
+            Console.Write("What do you guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicnumber > guess)
+            {
+                Console.Write("Higher ");
+            }
+            if (magicnumber < guess)
+            {
+                Console.Write("Lower ");
+            }
+            if (magicnumber == guess)
+            {
+                Console.Write($"You Got it its {magicnumber}");
+            }
+
+        }
+
     }
 }
