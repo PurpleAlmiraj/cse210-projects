50e44facde42bc202d094661490199c483eb52bb
Ryan Jones <ryanbluej@gmail.com>
2023-05-02 12:57:15 -0700

    W3 prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Astronaut";
+        job1._company = "NASA";
+        job1._startYear = 1995;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Chef";
+        job2._company = "AppleBees";
+        job2._startYear = 2020;
+        job2._endYear = 2021;
+
+        Resume myResume = new Resume();
+        myResume._name = "Joe Biddy";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
